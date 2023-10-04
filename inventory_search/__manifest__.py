dfa90f0533524923526dbed96ea4a56113917515
yenademir <116823062+yenademir@users.noreply.github.com>
2023-10-04 09:51:14 +0300

    inventory search added

---
+++ inventory_search/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Inventory Custom Search",
+    'version': '1.0',
+    'summary': "Enhanced search functionalities for the Inventory module",
+    'description': """
+        This add-on provides enhanced search functionalities to the Inventory module, 
+        making it easier and more efficient for users to locate items and manage their inventory.
+    """,
+    'author': "Emre Mataracı",
+    'category': 'Inventory',
+    'depends': ['Inventory'],
+    'data': [
+        'views/transfer_search_view.xml',
+        'views/product_search_view.xml'
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
+
