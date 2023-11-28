db19c888b7b7cfae09d9dcb879c0cde46ca61bec
yenademir <116823062+yenademir@users.noreply.github.com>
2023-11-28 09:18:32 +0300

    Update odoo_to_web_ip.py

--- Odoo_To_Web(InspectionPlan)/models/odoo_to_web_ip.py
+++ Odoo_To_Web(InspectionPlan)/models/odoo_to_web_ip.py
@@ -25,15 +25,15 @@ class PurchaseOrderLine(models.Model):
             data.append({
                 'vendor_odooid': record.partner_id.id,
                 'vendor_name': record.partner_id.name,
-                'customer_odooid': record.product_id.x_studio_customer.id,
-                'customer_name': record.product_id.x_studio_customer.name,
+                'customer_odooid': record.product_id.customer.id,
+                'customer_name': record.product_id.customer.name,
                 'project_number': record.account_analytic_id.name,
                 'product_odooid': record.product_id.id,
                 'product_name': record.product_id.name,
                 'order_id': record.order_id.id,
                 'order_number': record.order_id.name,
                 'quantity': record.product_qty,
-                'delivery_date': record.x_required_delivery_date.isoformat(),
+                'delivery_date': record.delivery_date.isoformat(),
                 'status': "Draft",
                 'state': "Open"
             })
