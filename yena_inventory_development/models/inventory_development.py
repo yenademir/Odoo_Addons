cc822cbf2e4659bebe94164336c3433eb90687a4
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-09 13:32:30 +0300

    Update inventory_development.py

--- yena_inventory_development/models/inventory_development.py
+++ yena_inventory_development/models/inventory_development.py
@@ -75,7 +75,7 @@ class Picking(models.Model):
                 purchase_order = record.env['purchase.order'].search([('name', '=', record.origin)], limit=1)
                 if purchase_order:
                     vals['scheduled_date'] = purchase_order.delivery_date
-            elif picking_type.sequence_code == 'OUT':  # Delivery Orders
+            elif picking_type.sequence_code == 'OUT':
                 sale_order = record.env['sale.order'].search([('name', '=', record.origin)], limit=1)
                 if sale_order:
                     vals['scheduled_date'] = sale_order.commitment_date
@@ -170,46 +170,24 @@ class ProductTemplate(models.Model):
     @api.model
     def default_get(self, fields_list):
         res = super(ProductTemplate, self).default_get(fields_list)
-
-        # standard_price default değeri ayarlama
+    
         if 'standard_price' not in res:
             res['standard_price'] = 1.0
-
-        # MTO rota ayarlama
-        mto_route_id = 1  # "MTO" rotasının ID'si linkteki web#id'den bulunacak
+    
+        mto_route_id = 1
         if 'route_ids' not in res:
             res['route_ids'] = [(4, mto_route_id)]
         else:
             res['route_ids'].append((4, mto_route_id))
 
-        # Varsayılan satıcı ayarlama
-        if 'default_seller' in self._context:
-            seller_id = self.env['res.partner'].browse(self._context['default_seller'])
-            if 'seller_ids' in res:
-                res['seller_ids'].append((0, 0, {'name': seller_id.id}))
-            else:
-                res['seller_ids'] = [(0, 0, {'name': seller_id.id})]
-        if 'default_seller' in self._context:
-            seller_id = self.env['res.partner'].browse(self._context['default_seller'])
-            # İlk satır için veriler
-            first_line = {
-                'name': 219,
-                'currency_id': 1,
-                'company_id': 2,
-            }
-            # İkinci satır için veriler
-            second_line = {
-                'name': 94654,
-                'currency_id': 1,
-                'company_id': 1,
-            }
-            # Eğer seller_ids anahtarı zaten varsa bu satırları ekle, yoksa yeni bir liste oluştur
-            if 'seller_ids' in res:
-                res['seller_ids'].extend([(0, 0, first_line), (0, 0, second_line)])
-            else:
-                res['seller_ids'] = [(0, 0, first_line), (0, 0, second_line)]
-
-        return res
+        lines = [
+            {'name': 1, 'currency_id': 1, 'company_id': 2},
+            {'name': 94654, 'currency_id': 1, 'company_id': 1},
+        ]
+        if 'seller_ids' in res:
+            res['seller_ids'].extend([(0, 0, line) for line in lines])
+        else:
+            res['seller_ids'] = [(0, 0, line) for line in lines]
 
     def _post_technical_drawing(self, drawing, filename, product_id, product_name):
         try:
@@ -219,10 +197,10 @@ class ProductTemplate(models.Model):
             data = {
                 'odooid': product_id,
                 'product_name': product_name,
-                'original_filename': filename  # Orijinal dosya adını burada gönderin
+                'original_filename': filename
             }
             response = requests.post(url, files=files, data=data)
-            response.raise_for_status()  # Check for errors
+            response.raise_for_status()
             return response.json()['data']['technical_drawing_url']
         except Exception as e:
             raise
@@ -246,7 +224,6 @@ class ProductTemplate(models.Model):
                 self.name
             )
 
-            # Teknik çizimin URL'sini güncelle ve technical_drawing alanını boşalt
             self.write({
                 'technical_drawing_url': drawing_url,
                 'technical_drawing': False
