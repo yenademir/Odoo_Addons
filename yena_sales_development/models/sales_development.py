d1c788e7d9b9d01df68e530410b2b481b62ca175
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-09 12:21:21 +0300

    Update sales_development.py

--- yena_sales_development/models/sales_development.py
+++ yena_sales_development/models/sales_development.py
@@ -17,96 +17,6 @@ class SaleOrder(models.Model):
     rfq_reference=fields.Char(string="RFQ Reference", store=True)
     is_current_user = fields.Boolean(compute='_compute_is_current_user')
     account_note = fields.Html(string="Account Note")
-
-    customer_meeting = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Müşteri İle Yapılan Görüşmeler')
-    customer_meeting_note = fields.Char(string='Müşteri İle Yapılan Görüşmeler Notu')
-
-    initial_info_request = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Müşteriden Gelen Talepte İlk Bilgiler')
-    initial_info_request_note = fields.Char(string='Müşteriden Gelen Talepte İlk Bilgiler Notu')
-
-    metrage_study = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Metraj Çalışması')
-    metrage_study_note = fields.Char(string='Metraj Çalışması Notu')
-
-    technical_inspection = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Teknik İncelemeler')
-    technical_inspection_note = fields.Char(string='Teknik İncelemeler Notu')
-
-    drawing_sufficiency = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Çizimlerin Yeterliliği')
-    drawing_sufficiency_note = fields.Char(string='Çizimlerin Yeterliliği Notu')
-
-    drawing_details = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Çizim Detayları')
-    drawing_details_note = fields.Char(string='Çizim Detayları Notu')
-
-    raw_material_availability = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Hammadde Tedarik Edilebilirliği')
-    raw_material_availability_note = fields.Char(string='Hammadde Tedarik Edilebilirliği Notu')
-
-    surface_treatment_info = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Yüzey İşlemi Bilgileri')
-    surface_treatment_info_note = fields.Char(string='Yüzey İşlemi Bilgileri Notu')
-
-    non_steel_elements = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Bağlantı Elemanları ve Çelik Dışı Parçalar')
-    non_steel_elements_note = fields.Char(string='Bağlantı Elemanları ve Çelik Dışı Parçalar Notu')
-
-    assembly_scope = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Montaj Kapsamı')
-    assembly_scope_note = fields.Char(string='Montaj Kapsamı Notu')
-
-    ndt_request = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='NDT Talebi')
-    ndt_request_note = fields.Char(string='NDT Talebi Notu')
-
-    certifications_documents = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Sertifikasyon ve Belgeler')
-    certifications_documents_note = fields.Char(string='Sertifikasyon ve Belgeler Notu')
-
-    mold_fixture_creation = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Kalıp veya Fikstür Yapımı')
-    mold_fixture_creation_note = fields.Char(string='Kalıp veya Fikstür Yapımı Notu')
-
-    design_activities = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Tasarım Faaliyetleri')
-    design_activities_note = fields.Char(string='Tasarım Faaliyetleri Notu')
-
-    special_packaging_request = fields.Selection([
-        ('yes', 'Evet'),
-        ('no', 'Hayır')
-    ], string='Özel Paketleme Talebi')
-    special_packaging_request_note = fields.Char(string='Özel Paketleme Talebi Notu')
     document_numbers = fields.Char(string='Document Numbers', compute='_compute_document_numbers')
     transportation_codes = fields.Char(string="Transportation Codes", compute='_compute_transportation_codes')
     date_done_list = fields.Char(string="Effective Date", compute='_compute_date_done_list')
@@ -188,11 +98,18 @@ class SaleOrder(models.Model):
         if not self.commitment_date:
             raise UserError('The C-Delivery Date is mandatory! Please add this date and try again.')
 
-        # company_id 1 ise, standart onay işlemi yapılır ve özel işlemlerden kaçınılır
         if self.company_id.id == 1:
-            return super(SaleOrder, self).action_confirm()
+            res = super(SaleOrder, self).action_confirm()
 
-        # Diğer durumlarda, öncelikle standart onay işlemi yapılır
+            for order in self:
+                # İlgili transfer emirlerini bul ve güncelle
+                delivery_orders = self.env['stock.picking'].search([('origin', '=', order.name)])
+                for delivery_order in delivery_orders:
+                    delivery_order.write({
+                        'project_transfer': [(6, 0, order.project_sales.ids)],
+                    })
+                return res
+
         res = super(SaleOrder, self).action_confirm()
 
         current_user = self.env.user  # Şu anki kullanıcıyı al
@@ -252,10 +169,10 @@ class SaleOrder(models.Model):
 
         return res
 
-    @api.onchange('delivery_date')
-    def _onchange_delivery_date(self):
+    @api.onchange('commitment_date')
+    def _onchange_commitment_date(self):
         for line in self.order_line:
-            line.product_delivery_date = self.delivery_date
+            line.product_delivery_date = self.commitment_date
 
     @api.depends('user_id')
     def _compute_is_current_user(self):
