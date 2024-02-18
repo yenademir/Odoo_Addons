94d2b82ab551f2777155200590aeb7cbd6d87a70
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-19 02:30:11 +0300

    Update purchase_development.py

--- yena_purchase_development/models/purchase_development.py
+++ yena_purchase_development/models/purchase_development.py
@@ -124,14 +124,35 @@ class PurchaseOrderLine(models.Model):
         ('metalworks', 'Metalworks'),
         ('attention', 'Attention'),
         ('attention_repair', 'Attention Repair'),
-        ('optional', 'Optional'),
-        ('hide', 'Hide'),
+        ('despatched', 'Despatched'),
+        ('partially_despatched', 'Partially Despatched'),
+        ('whoops', 'WHOOPS!'),
     ], string='Production Status')
 
-    @api.depends('product_qty', 'qty_received')
-    def _compute_line_status(self):
+    @api.depends('qty_received', 'product_qty')
+    def _compute_production_status(self):
         for record in self:
-            if record.product_qty > record.qty_received:
-                record.line_status = "ongoing"
-            elif record.product_qty == record.qty_received:
-                record.line_status = "finished"
+            if record.qty_received == record.product_qty:
+                record.production_status = 'despatched'
+            elif 0 < record.qty_received < record.product_qty:
+                record.production_status = 'partially_despatched'
+            elif record.qty_received > record.product_qty:
+                record.production_status = 'whoops'
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def write(self, vals):
+        res = super(StockMove, self).write(vals)
+        for move in self.filtered(lambda m: m.state == 'done' and m.purchase_line_id):
+            purchase_line = move.purchase_line_id
+            product_qty = purchase_line.product_qty
+            qty_received = sum(purchase_line.move_ids.filtered(lambda m: m.state == 'done').mapped('quantity_done'))
+
+            if qty_received == product_qty:
+                purchase_line.production_status = 'despatched'
+            elif 0 < qty_received < product_qty:
+                purchase_line.production_status = 'partially_despatched'
+            elif qty_received > product_qty:
+                purchase_line.production_status = 'whoops'
+        return res
