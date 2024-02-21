1fa1a6cdcdbcbab5fbae0afd681cfa3233e1ec63
yenademir <116823062+yenademir@users.noreply.github.com>
2024-02-21 08:53:08 +0300

    Update batch_transfer_model.py

--- batch_transfer_extension/models/batch_transfer_model.py
+++ batch_transfer_extension/models/batch_transfer_model.py
@@ -189,6 +189,11 @@ class StockPickingBatch(models.Model):
     def _inverse_transportation_code(self):
         for batch in self:
             batch.picking_ids.write({'transportation_code': batch.transportation_code})
+            
+   @api.depends('picking_ids.logistic_company')
+    def _inverse_logistic_company(self):
+        for batch in self:
+            batch.picking_ids.write({'logistic_company': batch.logistic_company})
 
     def _inverse_situation(self):
         for batch in self:
