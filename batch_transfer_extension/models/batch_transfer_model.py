b7aef123f746ec55df8edabb14877f903266cc1d
yenademir <116823062+yenademir@users.noreply.github.com>
2024-05-07 01:44:00 +0300

    Update batch_transfer_model.py

--- batch_transfer_extension/models/batch_transfer_model.py
+++ batch_transfer_extension/models/batch_transfer_model.py
@@ -138,7 +138,7 @@ class StockPickingBatch(models.Model):
             return res
 
     def send_batch_transfer_email(self):
-        if batch.picking_type_id.id == 2:
+        if self.picking_type_id.id == 2:
             template = self.env.ref('batch_transfer_extension.mail_template_batch_transfer_done')   
             template.send_mail(self.id, force_send=True)
         
